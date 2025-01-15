e07a567992e3195fa67e38488f236f530665f5ab
rudduddl <kim5472678@gmail.com>
2025-01-15 17:50:40 +0900

    day01 3번 실습

---
+++ src/main/java/com/nhnacademy/study/Main.java
@@ -0,0 +1,22 @@
+package com.nhnacademy.study;
+
+import com.beust.jcommander.JCommander;
+
+public class Main {
+    public static void main(String[] args){
+        CgiArgs cgiArgs = new CgiArgs();
+
+        JCommander.newBuilder()
+                .addObject(cgiArgs)
+                .build()
+                .parse(args);
+
+        System.out.println("content-type:"+cgiArgs.getContentType());
+        System.out.println("method:" + cgiArgs.getMethod());
+        System.out.println("query-string:" + cgiArgs.getQueryString());
+        System.out.println("server-name:" + cgiArgs.getServerName());
+        System.out.println("server-port:" + cgiArgs.getPort());
+        System.out.println("path:" + cgiArgs.getPath());
+        System.out.println("body:" + cgiArgs.getBody());
+    }
+}
